328f488dbd24861e65bc22d5bd25e0636628c3b8
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-12-11 18:40:08 +0100

    add error warning

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # ipyexcalidraw
-
+<!-- 
 ```sh
 pip install ipyexcalidraw
-```
+``` -->
+
+
+CURRENTLY NOT WORKING, 
+import { Excalidraw } from "@excalidraw/excalidraw";
+gives the error  
+
+"""
+Uncaught ReferenceError: process is not defined
+    at node_modules/@excalidraw/excalidraw/main.js 
+        """
+Made with
+
+https://github.com/excalidraw/excalidraw
+npm install react react-dom @excalidraw/excalidraw
--- js/widget.jsx
+++ js/widget.jsx
@@ -1,15 +1,22 @@
 import * as React from "react";
 import { createRender, useModelState } from "@anywidget/react";
 import "./widget.css";
+import { Excalidraw } from "@excalidraw/excalidraw";
 
 export const render = createRender(() => {
-	const [value, setValue] = useModelState("value");
-	return (
-		<button
-			className="ipyexcalidraw-counter-button"
-			onClick={() => setValue(value + 1)}
-		>
-			count is {value}
-		</button>
-	);
+  const [value, setValue] = useModelState("value");
+  return (
+    <>
+      <h1>Excalidraw</h1>
+      <div
+        style={{
+          position: "relative",
+          width: "20px",
+          height: "300px",
+        }}
+      >
+        {/* <Excalidraw /> */}
+      </div>
+    </>
+  );
 });
